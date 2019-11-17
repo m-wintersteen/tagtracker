75ee7bb21af3468ee9ecf330f1448cdbfc35265a
m-wintersteen <muniwintersteen@gmail.com>
2019-11-17 14:19:15 -0700

    Got database connection to work

--- index.php
+++ index.php
@@ -1,11 +1,29 @@
 <?php 
 
+include "./config/masterConfig.php";
 include './meta/inc/header.php';
+
+include_once './meta/assets/dbconnect.inc';
 
 ?>
 
 <div class="container">
     <p>Demo</p>
+    
+    <?php
+        echo "Hunter<br>";
+        //Getting a random selection of the database for the banner
+		$query = "
+			SELECT *
+			FROM Hunter";
+		$result = $dbConn->query($query) or die("Data query error");
+    
+		while ($row = $result->fetch_object()) {
+            echo $row->Hunter_id." ".$row->Fname." ".$row->Minit." ".$row->Lname." ".$row->Resident."<br>";
+            
+        }
+
+?>
 </div>
 
 <div class="clear"></div>
