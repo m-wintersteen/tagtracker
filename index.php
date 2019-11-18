848f5debca6aa5fd324d30c364b032c7317e220a
m-wintersteen <muniwintersteen@gmail.com>
2019-11-18 11:53:53 -0700

    Got Login Working

--- index.php
+++ index.php
@@ -8,22 +8,12 @@ include_once './meta/assets/dbconnect.inc';
 ?>
 
 <div class="container">
-    <p>Demo</p>
-    
-    <?php
-        echo "Hunter<br>";
-        //Getting a random selection of the database for the banner
-		$query = "
-			SELECT *
-			FROM Hunter";
-		$result = $dbConn->query($query) or die("Data query error");
-    
-		while ($row = $result->fetch_object()) {
-            echo $row->Hunter_id." ".$row->Fname." ".$row->Minit." ".$row->Lname." ".$row->Resident."<br>";
-            
-        }
-
-?>
+    <form action="profile.php" method="post">
+        Hunter ID: <input type="text" name="id">
+        <br>
+        Password: <input type="password" name="password">
+        <input type="submit" value="Submit">
+    </form>
 </div>
 
 <div class="clear"></div>
---
+++ profile.php
@@ -0,0 +1,46 @@
+<?php 
+
+include "./config/masterConfig.php";
+include './meta/inc/header.php';
+
+include_once './meta/assets/dbconnect.inc';
+
+session_start();
+
+?>
+<button onclick="window.location.href = 'logout.php';">Log Out</button>
+
+<div class="container">
+    <?php
+        if ( ! empty( $_POST ) ) {
+            $Hunter_id = $_POST['id'];
+            $h_password = $_POST['password'];
+
+            $sql = "SELECT Fname, Lname FROM Hunter WHERE Hunter_id='$Hunter_id' AND h_password='$h_password'";
+
+            $result = $dbConn->query($sql) or die("Data query error");
+
+            if($result-> num_rows > 0){
+                while($row=$result->fetch_assoc()){
+                    echo "Welcome Hunter! <br>".$row["Fname"]." ".$row["Lname"];
+                    $_SESSION['id'] = $Hunter_id;
+                    $_SESSION['Name'] = $row["Fname"]." ".$row["Lname"];
+                }
+            } else {
+                echo "No user found";
+            }
+        } else if(isset( $_SESSION['id'] )){
+            echo "Welcome Hunter! <br> ".$_SESSION['Name'];
+        } else {
+            header("Location: http://localhost:8080/tagtracker/");
+        }
+?>
+</div>
+
+<div class="clear"></div>
+
+<?php 
+
+include './meta/inc/footer.php';
+
+?>
